0025d27beb7f29853f29df1a197e3d23ff4fb5f9
Canto <jk5643@nyu.edu>
2020-08-17 20:41:36 -0700

    Create hello world rest controller

---
+++ src/main/java/com/spring/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.spring.restfulwebservices;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    // Need get method to convert to json, otherwise it will fail
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HelloWorldBean [message=%s]", message);
+    }
+}
---
+++ src/main/java/com/spring/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.spring.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    ///hello-world/path-variable/in28minutes
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
