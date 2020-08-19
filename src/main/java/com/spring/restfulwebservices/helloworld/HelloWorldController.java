2031f915c0893bacf906c6f74892b5c0a2221e37
Canto <jk5643@nyu.edu>
2020-08-18 23:28:20 -0700

    Implement internationalization

--- src/main/java/com/spring/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/spring/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.spring.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
-//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @Autowired
+    private MessageSource messageSource;
+
+    //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -20,5 +28,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null,
+                locale);
     }
 }
