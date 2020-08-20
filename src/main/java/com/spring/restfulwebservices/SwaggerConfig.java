1f49b0cf6ae629f14577892f9c1a33969740d777
Canto <jk5643@nyu.edu>
2020-08-19 18:17:39 -0700

    Enhance documentation

--- src/main/java/com/spring/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/spring/restfulwebservices/SwaggerConfig.java
@@ -2,19 +2,40 @@ package com.spring.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2WebMvc
 public class SwaggerConfig {
+
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Canto", "http://localhost.com:8080", "no_email@gmail.com");
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Rest Webservices API Title", "Rest Webservices API Description", "1.0",
+            "urn:tos", DEFAULT_CONTACT,
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String>(Arrays.asList("application/json",
+                    "application/xml"));
 
 //    http://localhost:8080/v2/api-docs
 //    But
 //    Not work for : http://localhost:8080/swagger-ui/ or http://localhost:8080/swagger-ui/index.html
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
